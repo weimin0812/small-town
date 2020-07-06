57313ecc688d6ede64be61fa00c03edd94ed1ece
weimin <weimin.0812@bytedance.com>
2020-07-06 10:26:34 +0800

    two sum

---
+++ leetcode/editor/en/Two Sum.go
@@ -0,0 +1,32 @@
+package en
+
+//Given an array of integers, return indices of the two numbers such that they a
+//dd up to a specific target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// Example: 
+//
+// 
+//Given nums = [2, 7, 11, 15], target = 9,
+//
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+// 
+// Related Topics Array Hash Table 
+// 👍 15534 👎 559
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if index, ok := m[target-num]; ok {
+			return []int{i, index}
+		}
+		m[num] = i
+	}
+	return nil
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
