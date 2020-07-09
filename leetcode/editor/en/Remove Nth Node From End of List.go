7229a1d22bd1c7f262e3db5f5ba0b0c13bc7be42
weimin <weimin.0812@bytedance.com>
2020-07-09 10:42:10 +0800

    Remove nth node from end of list

---
+++ leetcode/editor/en/Remove Nth Node From End of List.go
@@ -0,0 +1,47 @@
+package en
+
+//Given a linked list, remove the n-th node from the end of list and return its
+//head. 
+//
+// Example: 
+//
+// 
+//Given linked list: 1->2->3->4->5, and n = 2.
+//
+//After removing the second node from the end, the linked list becomes 1->2->3->
+//5.
+// 
+//
+// Note: 
+//
+// Given n will always be valid. 
+//
+// Follow up: 
+//
+// Could you do this in one pass? 
+// Related Topics Linked List Two Pointers 
+// 👍 3303 👎 228
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	ret := &ListNode{Next: head}
+	fast, slow := ret, ret
+	for i := 0; i <= n; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return ret.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
