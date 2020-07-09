314b715e7f4dde83406caa8f3549e0b57607b806
weimin <weimin.0812@bytedance.com>
2020-07-09 11:05:15 +0800

    merge two sorted lists

---
+++ leetcode/editor/en/Merge Two Sorted Lists.go
@@ -0,0 +1,45 @@
+package en
+
+//Merge two sorted linked lists and return it as a new sorted list. The new list
+// should be made by splicing together the nodes of the first two lists. 
+//
+// Example: 
+//
+// 
+//Input: 1->2->4, 1->3->4
+//Output: 1->1->2->3->4->4
+// 
+// Related Topics Linked List 
+// 👍 4255 👎 594
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	ret := &ListNode{}
+	current := ret
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+	if l1 != nil {
+		current.Next = l1
+	}
+	if l2 != nil {
+		current.Next = l2
+	}
+	return ret.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
